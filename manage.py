4a6053c4fb36a9d65b1a588c0a910a03ded74206
cjlg15 <vxfm219byc0052@126.com>
2021-05-24 11:40:07 +0800

    项目配置&数据库配置

--- manage.py
+++ manage.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目配置"""
+    DEBUG = True
+
+    # 为数据库添加配置
+    SQLAlchemy_DATABASE_URI = "mysql://root:135254@127.0.0.1:3306/flask_news"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
